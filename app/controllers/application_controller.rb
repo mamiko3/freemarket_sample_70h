19890d37ce9ab0119db1fa4ae3f78c5a857d42fb
mamiko3 <mmurakami722@gmail.com>
2020-03-09 17:41:27 +0900

    Summary(required)_userページdevise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :image, :first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number, :year_birth_at, :month_birth_at, :day_birth_at])
+  end
+
 end
---
+++ db/migrate/20200308151441_add_column_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+    add_column :users, :first_name_kana, :string, null: false
+    add_column :users, :last_name_kana, :string, null: false
+    add_column :users, :phone_number, :string, null: false, unique: true
+  end
+end
