c6424cc6d96f9ad4e13e156c6ac4ec7dfd248574
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2020-03-10 13:21:22 +0900

    items バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,12 @@
 class Item < ApplicationRecord
 has_many :images, dependent: :destroy
+
 belongs_to :category
 belongs_to :size
 belongs_to :brand
+validates :name,:price,:explain,:postage,:region,:condition,:shipping presence: true
+validates :name, length: { minimum: 40 }
+validates :name,length: {  maximum: 1000 }
+validates :price, presence: true, inclusion: 300..9999999
 end
 
