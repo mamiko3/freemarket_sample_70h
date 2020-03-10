ec928241f1af8bc0b0378bc7a2f1d600b7e42e38
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2020-03-10 14:09:35 +0900

    filed_for準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
 has_many :images, dependent: :destroy
+accepts_nested_attributes_for :images,allow_destroy: true
+accepts_nested_attributes_for :brands,allow_destroy: true
+accepts_nested_attributes_for :categorys,allow_destroy: true
+accepts_nested_attributes_for :sizes,allow_destroy: true
 
 belongs_to :category
 belongs_to :size
