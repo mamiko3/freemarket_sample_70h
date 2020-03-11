11f503a92bdcb3e7f51ab0fa1e9fef07fac19466
ta07ki <60640482+takimotokyousuke@users.noreply.github.com>
2020-03-11 11:59:10 +0900

    詳細ページ見た目(未完成)（出品ページ待）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_012544) do
+ActiveRecord::Schema.define(version: 2020_03_10_225116) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip", null: false
@@ -90,7 +90,6 @@ ActiveRecord::Schema.define(version: 2020_03_10_012544) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname", null: false
-    t.string "image"
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
