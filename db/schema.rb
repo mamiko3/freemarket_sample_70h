fb7ee8ec6304dc24ef417a3877c431c7b4de0429
ta07ki <60640482+takimotokyousuke@users.noreply.github.com>
2020-03-12 14:12:16 +0900

    ユーザーマイページ（仮）
    マークアップだけやりました。
    クレジットカードは後ほど変更、登録ページに飛ばします
    後ほどまた追加する予定です

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_152021) do
+ActiveRecord::Schema.define(version: 2020_03_10_012544) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_03_08_152021) do
     t.string "dst_last_name_kana", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
