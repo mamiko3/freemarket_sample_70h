69d2ba2477f8db630bed1ae50f5015a540ff5af6
mamiko3 <mmurakami722@gmail.com>
2020-03-12 20:24:44 +0900

    gem ancestry追加、categoriesテーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_114534) do
+ActiveRecord::Schema.define(version: 2020_03_12_112101) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_03_10_114534) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
