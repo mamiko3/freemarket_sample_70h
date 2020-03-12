a2e19ef663b19a815a85b8d2db6d2c83528a7310
ta07ki <60640482+takimotokyousuke@users.noreply.github.com>
2020-03-12 14:38:32 +0900

    変数名修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @nickname = current_user.nickname
-    @name1 = current_user.last_name 
-    @name2 = current_user.first_name
+    @last_name = current_user.last_name 
+    @first_name = current_user.first_name
   end
 end
