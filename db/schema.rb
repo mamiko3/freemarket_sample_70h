f5b10723df87427fb30dc37e14ebfeb8eb2a0b0a
mamiko3 <mmurakami722@gmail.com>
2020-03-13 19:59:36 +0900

    migrate remove_user_id:items

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_03_13_042708) do
+ActiveRecord::Schema.define(version: 2020_03_12_135448) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip", null: false
@@ -73,7 +72,6 @@ ActiveRecord::Schema.define(version: 2020_03_13_042708) do
     t.string "shipping", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.integer "buyer_id"
   end
 
