f1a795b5a0cf1a25d0eee99dad2ae0a53efd4de7
Hiroaki-Nakajima <h.19860820@gmail.com>
2020-03-13 21:41:54 +0900

    select_tagに記述変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_03_13_042708) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -73,8 +72,8 @@ ActiveRecord::Schema.define(version: 2020_03_13_042708) do
     t.string "shipping", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.integer "buyer_id"
+    t.integer "user_id"
   end
 
   create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
