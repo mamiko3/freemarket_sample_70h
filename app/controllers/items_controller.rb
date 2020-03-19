5c62d9980d714b721e5be5fa7b7faf29f62eecfa
ta07ki <60640482+takimotokyousuke@users.noreply.github.com>
2020-03-19 17:23:43 +0900

    修正完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,13 @@
 class ItemsController < ApplicationController
+
+  before_action :set_items, only: [:show,:edit,:update,:destroy]
   def index
     @items = Item.includes(:images).order("created_at DESC").limit(3)
     @parents = Category.all.order("id ASC").limit(13)
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
 
   def new
@@ -47,33 +49,39 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     @image = @item.images
     @prefectures=Prefecture.all
   
   end
 
   def update
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     @image = @item.images
-    if @item.user_id == current_user.id
-      @item.update(item_params)
-      redirect_to root_path
+    @item.user_id == current_user.id
+    if @item.update(item_params)
+      redirect_to items_path
     else
       render 'edit'
-
     end
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to root_path
+    # @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :price,:explain,:postage,:region,:condition,:shipping,images_attributes: [:image,:_destroy, :id]).merge(user_id: current_user.id)
+  end
+
+  def set_items
+    @item = Item.find(params[:id])
   end
 
 end
