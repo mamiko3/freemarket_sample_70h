836764099e8d6b2f2835e1377d27b9c09abcfb27
mamiko3 <mmurakami722@gmail.com>
2020-03-23 21:07:44 +0900

    エラーハンドリング、セキュリティ項目実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
 
   before_action :set_items, only: [:show,:edit,:update,:destroy]
+  
+
   def index
     @items = Item.includes(:images).order("created_at DESC").limit(10)
     @parents = Category.all.order("id ASC").limit(13)
@@ -38,32 +40,67 @@ class ItemsController < ApplicationController
 
   def create
     @prefectures=Prefecture.all
-
-
     @item = Item.new(item_params)
     @item.save
-
-    # if @item.save
-    #   redirect_to   root_path
-    # else
-    #   render "new"
-    # end
   end
 
   def edit
-    @image = @item.images
-    @prefectures=Prefecture.all
-    @items = Category.where(ancestry:nil)
-    @category_parent_array = ["選択してください"]
-    @category_children_array = ["選択してください"]
-    @category_grandchildren_array = ["選択してください"]
-    #データベースから、親カテゴリーのみ抽出し、配列化
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.name
-      @category_children_array << parent.name
-      @category_grandchildren_array << parent.name
-      @parents = Category.all.order("id ASC").limit(13)
+    if current_user.id == @item.user_id
+      @image = @item.images
+      @prefectures=Prefecture.all
+      @items = Category.where(ancestry:nil)
+      @category_parent_array = ["選択してください"]
+      @category_children_array = ["選択してください"]
+      @category_grandchildren_array = ["選択してください"]
+      #データベースから、親カテゴリーのみ抽出し、配列化
+      Category.where(ancestry: nil).each do |parent|
+        @category_parent_array << parent.name
+        @category_children_array << parent.name
+        @category_grandchildren_array << parent.name
+        @parents = Category.all.order("id ASC").limit(13)
+        end
+    else
+      redirect_to root_path
     end
+  end
+
+
+  def update
+    @image = @item.images
+    @item.user_id == current_user.id
+    if @item.update(item_params)
+      redirect_to items_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def search
+    @items = Item.search(params[:keyword])
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price,:explain,:postage,:region,:condition,:category_id,:shipping,images_attributes: [:image,:_destroy, :id]).merge(user_id: current_user.id)
+  end
+
+  def set_items
+    @item = Item.find(params[:id])
+  end
+
+end
+
+
+
 #     # @parents = Category.all.order("id ASC").limit(13)
 #     @selected_grandchild_category = @item.category.name
 #     @category_grandchildren_array = [{id: "---", name: "---"}]
@@ -85,43 +122,3 @@ class ItemsController < ApplicationController
 #       parent_hash = {id: "#{parent.id}", name: "#{parent.name}"}
 #       @category_parents_array << parent_hash
 # end
-end
-
-  def update
-    @image = @item.images
-    @item.user_id == current_user.id
-    if @item.update(item_params)
-      redirect_to items_path
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      render 'edit'
-    end
-  end
-
-
-  def search
-    @items = Item.search(params[:keyword])
-  end
-
-
-  
-
-  private
-  def item_params
-
-    params.require(:item).permit(:name, :price,:explain,:postage,:region,:condition,:category_id,:shipping,images_attributes: [:image,:_destroy, :id]).merge(user_id: current_user.id)
-
-  end
-
-  def set_items
-    @item = Item.find(params[:id])
-  end
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_105638) do
+ActiveRecord::Schema.define(version: 2020_03_23_073905) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip", null: false
@@ -74,9 +74,9 @@ ActiveRecord::Schema.define(version: 2020_03_20_105638) do
     t.datetime "updated_at", null: false
     t.integer "buyer_id"
     t.integer "user_id", null: false
-    t.bigint "category_id", null: false
     t.string "brand"
     t.string "size"
+    t.bigint "category_id"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
