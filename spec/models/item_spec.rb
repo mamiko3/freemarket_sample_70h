7eeb32cae7dd9db0430589d4ac1a27977c0c6a88
ta07ki <60640482+takimotokyousuke@users.noreply.github.com>
2020-03-23 11:17:20 +0900

    コミット

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 describe Item do
   describe '#create' do
-  it "is valid with a name,price,explain,postage,region,condition,shipping,category_id"do
+  it "is valid with a name,price,explain,postage,region,condition,shipping"do
     expect(build(:item)).to be_valid
   end
   it "is invalid without a name" do
@@ -43,13 +43,6 @@ describe Item do
     item.valid?
     expect(item.errors[:shipping]).to include("を入力してください")
   end
-
-  it "is invalid without a category_id" do
-    item = build(:item ,category_id: nil)
-    item.valid?
-    expect(item.errors[:category_id]).to include("を入力してください")
-  end
-
   it "is valid with a name that has less than 40 characters " do
     item = build(:item, name: "aaaaaa")
     expect(item).to be_valid
@@ -69,72 +62,4 @@ describe Item do
   expect(item).to be_valid
 end
 end
-  describe '#edit' do
-    it "is valid with a name,explain,postage,category_id,condition,shipping,region,condition,shipping,category_id" do
-      expect(build(:item)).to be_valid
-  end
-  it "is invalid without a name" do
-    item = build(:item,name: nil)
-    item.valid?
-    expect(item.errors[:name]).to include("を入力してください")
-  end
-  it "is invalid without a price" do
-    item = build(:item,price: nil)
-    item.valid?
-    expect(item.errors[:price]).to include("を入力してください", "は一覧にありません")
-  end
-  it "is invalid without a explain" do
-    item = build(:item, explain: nil)
-    item.errors
-    expect(item.errors[:explain]).to include()
-  end
-
-  it "is invalid without a postage" do
-    item = build(:item,postage: nil)
-    item.valid?
-    expect(item.errors[:postage]).to include("を入力してください")
-  end
-
-  it "is invalid without a region" do
-    item = build(:item,region: nil)
-    item.valid?
-    expect(item.errors[:region]).to include("を入力してください")
-  end
-
-  it "is invalid without a condition" do
-    item = build(:item, condition: nil)
-    item.valid?
-    expect(item.errors[:condition]).to include("を入力してください")
-  end
-
-  it "is invalid without a shipping" do
-    item = build(:item, shipping: nil)
-    item.valid?
-    expect(item.errors[:shipping]).to include("を入力してください")
-  end
-
-  it "is invalid without a category_id" do
-    item = build(:item ,category_id: nil)
-    item.valid?
-    expect(item.errors[:category_id]).to include("を入力してください")
-  end
-
-  it "is valid with a name that has less than 40 characters " do
-    item = build(:item, name: "aaaaaa")
-    expect(item).to be_valid
-  end
-
-  it "is valid with a explain that has less than 1000 characters " do
-    item = build(:item, explain: "")
-    expect(item).to be_valid
-  end
-
-  it "is valid with a explain that has less than 1000 characters " do
-    item = build(:item, explain: "aaaaaa")
-    expect(item).to be_valid
-  end
- it "is valid with a price that has less than inclusion: 300..9999999"do
-  item = build(:item, price: 300)
-  expect(item).to be_valid
-  end
 end
